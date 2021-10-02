b3bc5df72a2daf11a00ba3e964d81e1bf5081179
LasinduDilankamal <ldilankamal@gmail.com>
2021-10-02 18:39:47 +0530

    add new

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('kids-home app is running!');
+    expect(page.getTitleText()).toEqual('the-fred-hollows-foundation app is running!');
   });
 
   afterEach(async () => {
